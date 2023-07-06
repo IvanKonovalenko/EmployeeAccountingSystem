c9a5aa2a7a2c194f997f2ed173192ab3157c6b8d
IvanKonovalenko <ivangame67@gmail.com>
2023-07-06 15:54:52 +0300

    added login form

--- EmployeeAccountingSystem/Form1.cs
+++ EmployeeAccountingSystem/Form1.cs
@@ -1,8 +1,8 @@
 namespace EmployeeAccountingSystem
 {
-    public partial class Form1 : Form
+    public partial class LogInForm : Form
     {
-        public Form1()
+        public LogInForm()
         {
             InitializeComponent();
         }
